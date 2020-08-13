c90c87b942e041cd3849ab4f9990b5a17a98b636
AryanSharma5 <avisharmaindia07@gmail.com>
2020-08-13 22:24:00 +0530

    Day-13

---
+++ week-2/IteratorForCombinationGeneration.py
@@ -0,0 +1,27 @@
+class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        self.characters = characters
+        self.n = len(characters)
+        self.combinations = self.gen_combinations(self.n, combinationLength)
+        self.ind = len(self.combinations) - 1
+
+    def next(self) -> str:
+        s = ""
+        for i in range(self.n):
+            if self.combinations[self.ind][i] != "0":
+                s += self.characters[i]
+        self.ind -= 1
+        return s
+
+    def hasNext(self) -> bool:
+        return self.ind > -1 
+    
+    def gen_combinations(self, l, n):
+        end = int("1" * l, 2)
+        ans = []
+        for i in range(end + 1):
+            b = bin(i)[2:]
+            if b.count('1') == n:
+                ans.append(b.zfill(l))
+        return ans
