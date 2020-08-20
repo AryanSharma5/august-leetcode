b2aed2f86b4adc78b426e93a397ce70ae2c15a45
AryanSharma5 <avisharmaindia07@gmail.com>
2020-08-20 18:46:37 +0530

    Day-15

---
+++ week-3/nonOverlapping.py
@@ -0,0 +1,9 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        end, ans = float('-inf'), 0
+        for i in sorted(intervals, key=lambda x: x[1]):
+            if i[0] >= end: 
+                end = i[1]
+            else: 
+                ans += 1
+        return ans
